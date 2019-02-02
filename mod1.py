cf4b55a0879c9f7a7bf3ed64e614398f14e6d9e7
kkang <linux386@naver.com>
2019-02-03 08:31:37 +0900

    version one

--- mod1.py
+++ mod1.py
@@ -10,74 +10,70 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import datetime as dt
 from urllib.request import urlopen
+import sqlalchemy 
+import pymysql
+pymysql.install_as_MySQLdb() # MySQLdb와 호환 
+import MySQLdb
 
-def date_format(d):
-    d = str(d).replace('-', '.')
-    
-    yyyy = int(d.split('.')[0]) 
-    mm = int(d.split('.')[1])
-    dd = int(d.split('.')[2])
+now = dt.datetime.today().strftime('%Y-%m-%d')
+engine = sqlalchemy.create_engine('mysql+pymysql://kkang:leaf2027@localhost/stock?charset=utf8',encoding='utf-8')
+conn = engine.connect()
 
-    this_date= dt.date(yyyy, mm, dd)
-    return this_date
-    
-class excel:
-    
 
-    def kospi_stock_price(self):
-    
-        data = pd.read_excel('d:\\kospi_list.xlsx')
-
-        code_list = data['종목코드'].tolist()
-        code_list = [str(item).zfill(6) for item in code_list]
-        name_list = data['종목명'].tolist()
-
-        # 코스피 상장종목 전체
-        stock_dic = dict(list(zip(code_list,name_list)))
-
-        for code in stock_dic.keys():
-            df  = fdr.DataReader(code,'2019-01-01')
-            print(code,stock_dic[code])
-            df['Code'],df['Name'] = code,stock_dic[code]
-            df = df[['Code','Name','Open','High','Low','Volume','Close']]
-    
-            #df.to_excel('d:\\data_set\\kospi\\'+ stock_dic[code] +'.xlsx',engine = 'xlsxwriter')
-            print(df)
-            
-    def kosdaq_stock_price(self, start_date ,end_date):
-        self.start_date = start_date
-        self.end_date = end_date
-        data = pd.read_excel('d:\\kosdaq_list.xlsx')
-
-        code_list = data['종목코드'].tolist()
-        code_list = [str(item).zfill(6) for item in code_list]
-        name_list = data['종목명'].tolist()
-
-        # 코스피 상장종목 전체
-        stock_dic = dict(list(zip(code_list,name_list)))
-
-        for code in stock_dic.keys():
-            df  = fdr.DataReader(code,start_date,end_date)
-            print(code,stock_dic[code])
-            df['Code'],df['Name'] = code,stock_dic[code]
-            df = df[['Code','Name','Open','High','Low','Volume','Close']]    
-            #df.to_excel('d:\\data_set\\kosdaq\\'+ stock_dic[code] +'.xlsx',engine = 'xlsxwriter')
-            print(df)
-
+def excel_to_mysql(file_name):
         
+    df=pd.read_excel('d:\\'+ file_name)
+    df.index.name='Date'
+    #df.to_sql(name=file_name, con=engine, if_exists='append')
+    return df
+
+
+def get_stock_price_from_fdr(self, market,toward, start_date ,end_date = now):
+        
+    self.market = market
+    self.toward = toward
+            
+    if market == 'kospi':
+        data = pd.read_excel('d:\\kospi_list.xlsx')
+   
+    elif market=='kosdsq':
+        data = pd.read_excel('d:\\kosdsq_list.xlsx')
+
+              
+    code_list = data['종목코드'].tolist()
+    code_list = [str(item).zfill(6) for item in code_list]
+    name_list = data['종목명'].tolist()
+
+    # 코스피 상장종목 전체
+    stock_dic = dict(list(zip(code_list,name_list)))
+
+    for code in stock_dic.keys():
+        df  = fdr.DataReader(code,start_date,end_date)
+        print(code,stock_dic[code])
+        df['Code'],df['Name'] = code,stock_dic[code]
+        df = df[['Code','Name','Open','High','Low','Volume','Close']]
+        if toward == 'excel':
+            df.to_excel('d:\\data_set\\kospi\\'+ stock_dic[code] +'.xlsx',engine = 'xlsxwriter')
+        elif toward == 'sql':
+            df.to_sql(name=market, con=engine, if_exists='append')
+        print(df)
+
+
+class to_excel:
+    
     def get_money_trend(self):
     
         path = 'd:\\money_trend.xlsx'
 
         url = 'http://finance.naver.com/sise/sise_deposit.nhn?&page='    
-        Data = pd.DataFrame(columns = ['고객예탁금', '신용잔고','주식형 펀드','혼합형 펀드','채권형 펀드'])
+        Data = pd.DataFrame(columns = ['고객예탁금', '신용잔고','주식형펀드','혼합형펀드','채권형펀드'])
         date_list = []
     
         # 값을 받을 사전
-        dictionary = {'고객예탁금': [],'신용잔고': [],'주식형 펀드': [],'혼합형 펀드': [],'채권형 펀드': []}
+        dictionary = {'고객예탁금': [],'신용잔고': [],'주식형펀드': [],'혼합형펀드': [],'채권형펀드': []}
 
         # dictionary key 인덱싱을 위한 리스트
-        name_list = ['고객예탁금','신용잔고','주식형 펀드','혼합형 펀드','채권형 펀드']
+        name_list = ['고객예탁금','신용잔고','주식형펀드','혼합형펀드','채권형펀드']
 
         # count mask
         mask = [0,1,3,5,7,9]
@@ -109,6 +105,11 @@ class excel:
                                 
                                     df = pd.DataFrame(dictionary,index = date_list)
                                     df = df.sort_index()
+                                    df.고객예탁금 = df.고객예탁금.str.replace(',','')
+                                    df.신용잔고 = df.신용잔고.str.replace(',','')
+                                    df.주식형펀드 = df.주식형펀드.str.replace(',','')
+                                    df.혼합형펀드 = df.혼합형펀드.str.replace(',','')
+                                    df.채권형펀드 = df.채권형펀드.str.replace(',','')
                                     df.to_excel(path, encoding='utf-8')
                                 
                                     return df
@@ -120,7 +121,7 @@ class excel:
                         count += 1
             
                 # 누락된 값을 발견하면 (펀드 자료에 누락된 값이 존재함)
-                if len(dictionary['고객예탁금']) != len(dictionary['주식형 펀드']):
+                if len(dictionary['고객예탁금']) != len(dictionary['주식형펀드']):
                     print(str(i)+ '번째 페이지에서 누락된 값 발생')
                     print('누락된 데이터를 제거합니다')
                     
@@ -131,20 +132,26 @@ class excel:
 
             print(str(i) + '번째 페이지 크롤링 완료')
 
-    def get_money_trend_date(self,until_date='00-12-27'):
+    def get_money_trend_date(self,until_date='2000-12-27'):
+        
+        year = until_date.split('-')[0]
+        mm = until_date.split('-')[1]
+        dd = until_date.split('-')[2]
+        year=year[2:]
+        until_date = year+'-'+mm+'-'+dd
     
         path = 'd:\\money_trend.xlsx'
         print(path)
 
         url = 'http://finance.naver.com/sise/sise_deposit.nhn?&page='    
-        Data = pd.DataFrame(columns = ['고객예탁금', '신용잔고','주식형 펀드','혼합형 펀드','채권형 펀드'])
+        Data = pd.DataFrame(columns = ['고객예탁금', '신용잔고','주식형펀드','혼합형펀드','채권형펀드'])
         date_list = []
     
         # 값을 받을 사전
-        dictionary = {'고객예탁금': [],'신용잔고': [],'주식형 펀드': [],'혼합형 펀드': [],'채권형 펀드': []}
+        dictionary = {'고객예탁금': [],'신용잔고': [],'주식형펀드': [],'혼합형펀드': [],'채권형펀드': []}
 
         # dictionary key 인덱싱을 위한 리스트
-        name_list = ['고객예탁금','신용잔고','주식형 펀드','혼합형 펀드','채권형 펀드']
+        name_list = ['고객예탁금','신용잔고','주식형펀드','혼합형펀드','채권형펀드']
 
         # count mask
         mask = [0,1,3,5,7,9]
@@ -171,11 +178,16 @@ class excel:
                                     date_list.append(date_)
                         
                                 #마지막 페이지인 경우
-                                    if date_ == until_date or i > 3:
+                                    if date_ <= until_date or i > 3:
                                         print(str(i-1) + '번째 페이지에서 크롤링 종료')
                                         date_list.pop(-1)
                                         df = pd.DataFrame(dictionary,index = date_list)
                                         df = df.sort_index()
+                                        df.고객예탁금 = df.고객예탁금.str.replace(',','')
+                                        df.신용잔고 = df.신용잔고.str.replace(',','')
+                                        df.주식형펀드 = df.주식형펀드.str.replace(',','')
+                                        df.혼합형펀드 = df.혼합형펀드.str.replace(',','')
+                                        df.채권형펀드 = df.채권형펀드.str.replace(',','')
                                         df.to_excel(path, encoding='utf-8')
                                 
                                         return df
@@ -187,7 +199,7 @@ class excel:
                         count += 1
             
                 # 누락된 값을 발견하면 (펀드 자료에 누락된 값이 존재함)
-                if len(dictionary['고객예탁금']) != len(dictionary['주식형 펀드']):
+                if len(dictionary['고객예탁금']) != len(dictionary['주식형펀드']):
                     print(str(i)+ '번째 페이지에서 누락된 값 발생')
                     print('누락된 데이터를 제거합니다')
                     
@@ -197,8 +209,74 @@ class excel:
                 
             print(str(i) + '번째 페이지 크롤링 완료')
             
+    def get_kpi_200(self,until_date='1995-12-27'):
+    
+        path = 'd:\\kpi_200.xlsx'
+        
+        url = 'https://finance.naver.com/sise/sise_index_day.nhn?code=KPI200&page='
+        Data = pd.DataFrame(columns = ['KPI200','거래량'])
+        date_list = []
+    
+        # 값을 받을 사전
+        dictionary = {'KPI200': [],'거래량': []}
+
+        # dictionary key 인덱싱을 위한 리스트
+        name_list = ['KPI200','거래량']
+
+        # count mask
+        mask = [0,1,5]
+    
+        for i in range(1,700):
+            source = urlopen(url+ str(i)).read()
+            source = BeautifulSoup(source,'lxml')
+
+            tbody = source.find('div',{'class':'box_type_m'})
+            trs = tbody.find_all('tr')
+            for tr in trs:
+                tds = tr.find_all('td')
+                count = 0
+    
+                for td in tds:
+                    # 변화량 제외하고 잔고량만 가져오기
+                    if not td.text.strip() == 0:
+                        if len(td.text.strip()) >= 4:
             
-    def get_kpi_200_date(self,until_date='00-12-27'):
+                            if count == 0:
+                                date_ = td.text.strip().replace('.','-')
+                        
+                                if not date_ in date_list :
+                                    date_list.append(date_)
+                                    
+                                #마지막 페이지인 경우
+                                else:
+                                    print(str(i-1) + '번째 페이지에서 크롤링 종료')
+                                    df = pd.DataFrame(dictionary,index = date_list)
+                                    df = df.sort_index()
+                                    df.거래량 = df.거래량.str.replace(',','')
+                                    df.to_excel(path, encoding='utf-8')
+                                
+                                    return df
+
+                            elif count in mask:
+                                temp = int(count/5)
+                                dictionary[name_list[temp]].append(td.text.strip())
+                                
+                        #print(count)
+                        count += 1
+            
+                # 누락된 값을 발견하면 (펀드 자료에 누락된 값이 존재함)
+                if len(dictionary['KPI200']) != len(dictionary['거래량']):
+                    print(str(i)+ '번째 페이지에서 누락된 값 발생')
+                    print('누락된 데이터를 제거합니다')
+                    
+                    date_list.pop(-1)
+                    dictionary['KPI200'].pop(-1)
+                    dictionary['거래량'].pop(-1)
+                
+            print(str(i) + '번째 페이지 크롤링 완료')
+            
+            
+    def get_kpi_200_date(self,until_date='1995-12-27'):
     
         path = 'd:\\kpi_200.xlsx'
         
@@ -237,11 +315,12 @@ class excel:
                                     date_list.append(date_)
                                     
                                     #마지막 페이지인 경우
-                                    if date_ == until_date or i > 3:
+                                    if date_ <= until_date or i > 3:
                                         print(str(i-1) + '번째 페이지에서 크롤링 종료')
                                         date_list.pop(-1)
                                         df = pd.DataFrame(dictionary,index = date_list)
                                         df = df.sort_index()
+                                        df.거래량 = df.거래량.str.replace(',','')
                                         df.to_excel(path, encoding='utf-8')
                                 
                                         return df
@@ -263,6 +342,9 @@ class excel:
                     dictionary['거래량'].pop(-1)
                 
             print(str(i) + '번째 페이지 크롤링 완료')
-    
+            
+
+in __name__ == "__main__":
+    print("This is Module")
     
     
